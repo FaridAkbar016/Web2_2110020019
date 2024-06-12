650c578b5e527b0411e3f119adf81b4bd57a2825
FaridAkbar016 <156918891+FaridAkbar016@users.noreply.github.com>
2024-06-13 00:58:09 +0800

    export database

--- 2110020019/db/db_2110020019.sql
+++ 2110020019/db/db_2110020019.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 12, 2024 at 04:31 PM
+-- Generation Time: Jun 12, 2024 at 04:55 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
